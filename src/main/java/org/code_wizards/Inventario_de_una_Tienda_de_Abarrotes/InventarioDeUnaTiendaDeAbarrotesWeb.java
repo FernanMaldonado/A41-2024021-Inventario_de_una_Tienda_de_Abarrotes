86e75d7dc873b2e5d9687ece35f2d24ba2b645b8
FernanMaldonado <fmaldonado-2024021@kinal.edu.gt>
2025-08-26 20:08:22 -0600

    Cambios a modulo de consola a Web

---
+++ src/main/java/org/code_wizards/Inventario_de_una_Tienda_de_Abarrotes/InventarioDeUnaTiendaDeAbarrotesWeb.java
@@ -0,0 +1,11 @@
+package org.code_wizards.Inventario_de_una_Tienda_de_Abarrotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventarioDeUnaTiendaDeAbarrotesWeb {
+    public static void main(String[] args) {
+        SpringApplication.run(InventarioDeUnaTiendaDeAbarrotesWeb.class, args);
+    }
+}
